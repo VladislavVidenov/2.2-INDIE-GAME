8a1402af1cd20f407ade5aee6148707152588c36
VladislavVidenov <vladislavvidenov95@gmail.com>
2015-11-14 23:32:37 +0100

    Update 0.2 - Movement Control

---
+++ 2.2 Indie game/Assets/Models + Textures/PlayerMovement.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+ 
+    
+    //Components
+    Rigidbody rigidBody;
+    
+
+    //Variables
+    [SerializeField]
+    float walkSpeed;
+    [SerializeField]
+    float crouchSpeed;
+
+    bool grounded = false;
+    bool canJump = true;
+    float speed = 10;
+    float maxVelocityClamp = 10f;
+
+    Vector3 input;
+    Vector3 targetVel;
+
+    // Use this for initialization
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        input = new Vector3(Input.GetAxis("Horizontal"),0,Input.GetAxis("Vertical"));
+        input *= (input.x == 1 && input.z == 1) ? 0.7f : 1.0f;
+
+        if (grounded)
+        {
+            targetVel = input;
+            targetVel = transform.TransformDirection(targetVel);
+            targetVel *= speed;
+
+            Vector3 currVel = rigidBody.velocity;
+            Vector3 newVel = targetVel - currVel;
+            newVel.x = Mathf.Clamp(newVel.x, -maxVelocityClamp, maxVelocityClamp);
+            newVel.y = 0;
+            newVel.z = Mathf.Clamp(newVel.z, -maxVelocityClamp, maxVelocityClamp);
+            rigidBody.AddForce(newVel, ForceMode.VelocityChange);
+        }
+
+
+
+        grounded = false;
+    }
+
+
+
+    void OnCollisionStay(Collision collision)
+    {
+        foreach(ContactPoint contact in collision.contacts)
+        {
+            
+            if (Vector3.Angle(contact.normal, Vector3.up) < 45)
+            {
+                grounded = true;
+            }
+        }
+    }
+
+}
