70961b7518779ec0f1762aa0bcc703ce0dc9c9b3
Kameloter <kameloter@hotmail.nl>
2015-11-23 00:08:07 +0100

    0.6.1
    tiled implemented for level editing/loading

---
+++ 2.2 Indie game/Assets/Editor/LoadLevelEditor.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(LoadLevelGame))]
+public class LoadLevelEditor : Editor {
+
+	public override void OnInspectorGUI () {
+		DrawDefaultInspector ();
+
+		LoadLevelGame loadlevel = (LoadLevelGame)target;
+
+		if (GUILayout.Button("Build")) {
+			loadlevel.LoadLevel();
+		}
+		if (GUILayout.Button("Delete")) {
+			loadlevel.DeleteScene();
+		}
+	}
+}
---
+++ 2.2 Indie game/Assets/Scripts/LoadLevelGame.cs
@@ -0,0 +1,130 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+public class LoadLevelGame : MonoBehaviour {
+	public string levelName;
+	
+	/// <summary>
+	/// The tileset, as an array.
+	/// </summary>
+	public GameObject[] tiles;
+
+	List<GameObject> levelObjects = new List<GameObject>();
+
+	int id= 0;
+
+	int rotationX;
+	int rotationY;
+	int rotationZ;
+
+	int [,] array;
+
+	int x=0;
+	int z=0;
+
+	int tileHeight= 0;
+
+	public int height = 25;
+	public int width = 25;
+	
+	//start loading the level at the start
+	void Start () {
+		array = new int[width, height];
+		LoadLevel();
+	}
+	
+	public void LoadLevel() {
+
+		//open xml
+		XmlReader xmlReader = XmlReader.Create(Application.streamingAssetsPath + "/" + levelName + ".tmx");
+		
+		//keep reading until end-of-file
+		while (xmlReader.Read()) {
+		
+			if (xmlReader.IsStartElement ()) {
+
+				switch (xmlReader.Name) {
+
+				case "tile":
+					if (xmlReader ["id"] != null) {
+						id = int.Parse (xmlReader ["id"]);
+						Debug.Log (id);
+					} else{
+						int index = int.Parse(xmlReader["gid"]);
+						InstatiateGameObject(xmlReader, index);
+					}
+
+					break;
+
+				case "property":
+					string attribute = xmlReader ["name"];
+
+					Debug.Log (attribute);
+
+					switch (attribute) {
+					
+					case "rotationX":
+						rotationX = int.Parse (xmlReader ["value"]);
+						Debug.Log (rotationX);
+
+						break;
+
+					case "rotationY":
+						rotationY = int.Parse (xmlReader ["value"]);
+						Debug.Log (rotationY);
+						
+						break;
+
+					case "rotationZ":
+						rotationZ = int.Parse (xmlReader ["value"]);
+						Debug.Log (rotationZ);
+						
+						break;
+
+					}
+
+					tiles[id].transform.rotation = Quaternion.Euler(rotationX,rotationY,rotationZ);
+					break;
+
+				case "layer":
+					tileHeight++;
+					x=0;
+					z=0;
+					Debug.Log(x);
+					break;
+				
+				}
+
+			}
+
+
+		}
+
+		tileHeight = 0;
+	}
+
+	void InstatiateGameObject (XmlReader xmlReader, int gameObjectIndex) {
+		if (int.Parse(xmlReader["gid"]) != 0){
+			GameObject go = GameObject.Instantiate(tiles[gameObjectIndex],new Vector3(width - x,tileHeight, z),tiles[gameObjectIndex].transform.rotation) as GameObject;
+			go.transform.SetParent(this.transform);
+			levelObjects.Add(go);
+		}
+		x++;
+		if(x>width-1) {
+			x=0;
+			z++;
+		}
+	}
+
+	public void DeleteScene () {
+		foreach(GameObject levelObject in levelObjects){
+			DestroyImmediate (levelObject);
+		}
+	}
+
+}
