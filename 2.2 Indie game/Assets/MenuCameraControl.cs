b69962ba60befa8e703e988008a4a7516ffb1a9b
Martijn Snip <martijnsnip@gmail.com>
2015-11-24 13:57:38 +0100

    MenuScene 2.01

---
+++ 2.2 Indie game/Assets/MenuCameraControl.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuCameraControl : MonoBehaviour {
+
+    public Transform currentMount;
+    public float cameraLerpSpeed = 0.1f;
+
+    void Update() {
+        transform.position = Vector3.Lerp(transform.position, currentMount.position, cameraLerpSpeed);
+        transform.rotation = Quaternion.Slerp(transform.rotation, currentMount.rotation, cameraLerpSpeed);
+    }
+
+    public void SetMount(Transform newMount) {
+        currentMount = newMount;
+    }
+
+}
---
+++ 2.2 Indie game/Assets/Scripts/UI/MenuScript.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuScript : MonoBehaviour {
+    void Start()
+    {
+        Debug.Log("I am menu script and i am useless af. atm.");
+    }
+
+  
+}
