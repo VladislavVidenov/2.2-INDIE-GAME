592e9756e4cb4d28824b06027af9f76c2edad345
Martijn Snip <martijnsnip@gmail.com>
2015-11-25 16:44:27 +0100

    MenuScene 2.04

---
+++ 2.2 Indie game/Assets/QualitySettingsScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class QualitySettingsScript : SettingsScript {
+    public bool expensiveChanges = true;
+
+    private string[] qualityLevels;
+    private int currentQualityLevel;
+
+    [Tooltip("Add all quality toggles from the 'Lowest' to the 'Best' setting.")]
+    public Toggle[] qualityToggles;
+
+    void Start() {
+        qualityLevels = QualitySettings.names;                                      //Check available quality settings and store them in an array;
+        currentQualityLevel = QualitySettings.GetQualityLevel();                    //Check the current quality setting and store it in a variable;
+
+        qualityToggles[currentQualityLevel].isOn = true;
+    }
+
+    public void SetQualitySetting(int qualitySettingIndex, GameObject toggleGo) {
+        QualitySettings.SetQualityLevel(qualitySettingIndex, expensiveChanges);
+        currentQualityLevel = qualitySettingIndex;
+    }
+
+    //Overloading
+    public void SetQualitySetting(string qualitySettingName) {
+        for (int i = 0; i < qualityLevels.Length; i++) {
+            if (qualityLevels[i] == qualitySettingName) {
+                QualitySettings.SetQualityLevel(i, expensiveChanges);
+                currentQualityLevel = i;
+            }
+        }
+    }
+}
