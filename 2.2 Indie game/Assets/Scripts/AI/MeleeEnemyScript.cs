de5e7960ed18b6da78d82acfad43513a52767d39
Kameloter <kameloter@hotmail.nl>
2015-11-28 20:12:57 +0100

    Update 0.8.2
    added some stuff

---
+++ 2.2 Indie game/Assets/Scripts/AI/MeleeEnemyScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MeleeEnemyScript : EnemyScript {
+	
+	// Use this for initialization
+	void Start () {
+		state = AIState.Running;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	  switch (state) {
+		case AIState.Attacking:
+			Attacking();
+			break;
+
+		case AIState.Running:
+			agent.SetDestination(player.position);
+			break;
+		}
+	}
+
+	void Attacking () {
+
+	}
+
+	void OnTriggerEnter (Collider other) {
+		if (other.CompareTag (Tags.player)) {
+			print ("helooo");
+		}
+	}
+}
