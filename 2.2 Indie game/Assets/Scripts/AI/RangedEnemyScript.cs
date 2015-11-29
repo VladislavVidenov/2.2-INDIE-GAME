52e220a0a6204b3077798d472f10251ec419d475
Martijn Snip <martijnsnip@gmail.com>
2015-11-29 16:34:48 +0100

    Update 0.8.3

---
+++ 2.2 Indie game/Assets/Scripts/AI/RangedEnemyScript.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RangedEnemyScript : EnemyScript {
+    Vector3 fightingPosition;
+    bool relocating;
+    float attackTimer;
+    float attackTime;
+
+
+    void Start() {
+        state = AIState.Attacking;
+    }
+
+    void Update() {
+        switch (state) {
+            case AIState.Attacking:
+                Attacking();
+                break;
+        }
+    }
+
+    void Attacking() {
+        if (!relocating) FindFightingPosition();
+        if (agent.remainingDistance <= agent.stoppingDistance) relocating = false;
+        this.transform.rotation = Quaternion.RotateTowards(this.transform.rotation, Quaternion.LookRotation(player.transform.position - this.transform.position), attackRotationSpeed);
+
+        if (Time.time - attackTimer > attackTime) {
+            Shoot();
+            attackTimer = Time.time;
+        }
+    }
+
+    void Shoot() {
+        //magic
+    }
+
+
+    void FindFightingPosition() {
+        if (Random.Range(0, 50) == 4) {
+            agent.Resume();
+            agent.updateRotation = false;
+
+            Vector3 delta = (this.transform.position - player.transform.position);
+
+            fightingPosition = player.transform.position + new Vector3(Random.Range(-7f, 7f), 0, Random.Range(-7f, 7f));
+            Vector3 dirToPoint = fightingPosition - player.transform.position;
+            if (Vector3.Dot(delta, dirToPoint) > 0) {
+                RaycastHit hit;
+                Vector3 dir = (player.transform.position - fightingPosition);
+
+                if (Physics.Raycast(fightingPosition, dir, out hit, 11f)) {
+                    Debug.DrawRay(fightingPosition, dir, Color.black, 5f);
+                    if (hit.collider.CompareTag(Tags.player)) {
+                        Debug.Log("I FOUND IT");
+                        agent.SetDestination(fightingPosition);
+                        relocating = true;
+                    }
+                }
+            }
+        }
+        else {
+            agent.Stop();
+        }
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if (other.CompareTag(Tags.player)) {
+            print("helooo");
+        }
+    }
+}
