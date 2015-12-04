7a7f5d660535bd6b27c26a47c41d9d29044ec2c7
Martijn Snip <martijnsnip@gmail.com>
2015-12-04 17:34:27 +0100

    Update 0.8.845
    Added an effect for the brightness and moved some stuff around.

--- 2.2 Indie game/Assets/Scripts/UI/GraphicsSettingsScript.cs
+++ 2.2 Indie game/Assets/Scripts/UI/GraphicsSettingsScript.cs
@@ -1,20 +1,35 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using UnityStandardAssets.ImageEffects;
 
-public class QualitySettingsScript : SettingsScript {
+public class GraphicsSettingsScript : SettingsScript {
+    [Header("Brightness")]
+    public Toggle customBrightness;
+    public Slider brightnessSlider;
+    public float middleGray = 0.1f;
+    
+    private Tonemapping toneMappingScript;
+
+
+    [Header("Quality")]
     public bool expensiveChanges = true;
-
     private string[] qualityLevels;
     private int currentQualityLevel;
 
     [Tooltip("Add all quality toggles from the 'Lowest' to the 'Best' setting.")]
     public Toggle[] qualityToggles;
 
+    
+
     void Awake() {
+        //Brightness
+        toneMappingScript = Camera.main.GetComponent<Tonemapping>();
+        toneMappingScript.middleGrey = middleGray;
+        
+        //Quality
         qualityLevels = QualitySettings.names;                                      //Check available quality settings and store them in an array;
         currentQualityLevel = QualitySettings.GetQualityLevel();                    //Check the current quality setting and store it in a variable;
-
         qualityToggles[currentQualityLevel].isOn = true;
     }
 
