c9afccd5a8a119654be0c8d5a0a6603c3ea3b5ec
Martijn Snip <martijnsnip@gmail.com>
2016-01-29 16:25:20 +0100

    Update 0.9.01 - Upgrades

---
+++ 2.2 Indie game/Assets/Scripts/Upgrades/Player/UpJumpHeight.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UpJumpHeight : Upgrade {
+
+
+    public override void Apply() {
+        print(string.Format("UPGRADE UNLOCKED: '{0}'", upgradeName));
+
+    }
+}
---
+++ 2.2 Indie game/Assets/Scripts/Upgrades/Player/UpSprintSpeed.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UpSprintSpeed : Upgrade {
+
+
+    public override void Apply() {
+        print(string.Format("UPGRADE UNLOCKED: '{0}'", upgradeName));
+
+    }
+}
