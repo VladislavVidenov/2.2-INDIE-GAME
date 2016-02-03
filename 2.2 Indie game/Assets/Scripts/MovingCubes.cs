0421cfec651064d35200aa27957c13821d465630
VladislavVidenov <vladislavvidenov95@gmail.com>
2016-02-03 13:49:14 +0100

    Update 0.9.28

---
+++ 2.2 Indie game/Assets/Scripts/MovingCubes.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingCubes : MonoBehaviour {
+
+	Light myLight;
+	Animator myAnimator;
+	// Use this for initialization
+	void Start () {
+		myAnimator = GetComponent<Animator> ();
+		myLight = GetComponentInChildren<Light> ();
+		myLight.intensity = 0;
+		myAnimator.speed = 0;
+		//myAnimator.enabled = false;
+	}
+	void MoveAnimation()
+	{
+		myLight.intensity = 8;
+		myAnimator.speed = 8;
+		Invoke ("StopAnimator", 0.2f);
+	}
+	void StopAnimator()
+	{
+		myLight.intensity = 0;
+		myAnimator.speed = 0;
+	}
+
+	void OnEnable()
+	{
+		WeaponScript.OnPistolShoot += MoveAnimation;
+		print("Subscribed !");
+	}
+	
+	
+	void OnDisable()
+	{
+		WeaponScript.OnPistolShoot -= MoveAnimation;
+		print("UnSubscribed !");
+	}
+}
