83688467464f8ea459dc8fa76229a057ab427ae2
Martijn Snip <martijnsnip@gmail.com>
2016-02-05 03:14:50 +0100

    Update 0.9.67 - Pickables

---
+++ 2.2 Indie game/Assets/Scripts/ArmorPickable.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArmorPickable : MonoBehaviour {
+    PlayerScript player;
+    void Start() {
+        player = GameManager.Instance.playerScript;
+    }
+
+	void OnTriggerEnter(Collider hit)
+    {
+        if (hit.CompareTag("Player")) {
+            player.IncreasePlayerStats(15, 0, 0, 0, 0);
+
+            Destroy(gameObject);
+        }
+      
+    }
+}
