ee797538f006dedb7edd5cb5cf61c470b1b16453
VladislavVidenov <vladislavvidenov95@gmail.com>
2016-02-05 13:57:12 +0100

    Update 0.9.74 - player death animation no fadeout

---
+++ 2.2 Indie game/Assets/DeathAnimation.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeathAnimation : MonoBehaviour {
+
+
+    public void Die()
+    {
+        this.transform.parent = null;
+        GetComponentInChildren<CameraMouseControl>().enabled = false;
+        GetComponentInChildren<PlayerActionScript>().enabled = false;
+        gameObject.AddComponent<Rigidbody>();
+        gameObject.AddComponent<SphereCollider>();
+        GetComponent<SphereCollider>().radius = 0.3f;
+        GetComponent<Rigidbody>().AddTorque(transform.right + transform.up);
+
+    }
+}
