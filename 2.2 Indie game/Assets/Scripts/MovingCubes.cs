6d8f70acd20b69503e84ffc6f07666b180b0c21f
VladislavVidenov <vladislavvidenov95@gmail.com>
2016-02-05 11:31:06 +0100

    Update 0.9.70

--- 2.2 Indie game/Assets/Scripts/MovingCubes.cs
+++ 2.2 Indie game/Assets/Scripts/MovingCubes.cs
@@ -3,26 +3,30 @@ using System.Collections;
 
 public class MovingCubes : MonoBehaviour {
 
-	Light myLight;
+	Light[] myLights;
 	Animator myAnimator;
 	// Use this for initialization
 	void Start () {
 		myAnimator = GetComponent<Animator> ();
-		myLight = GetComponentInChildren<Light> ();
-		myLight.intensity = 0;
-		myAnimator.speed = 0;
+		myLights = GetComponentsInChildren<Light> ();
+		myLights[0].intensity = 0;
+        myLights[1].intensity = 0;
+        myAnimator.speed = 0;
 		//myAnimator.enabled = false;
 	}
-	void MoveAnimation()
-	{
-		myLight.intensity = 8;
-		myAnimator.speed = 8;
-		Invoke ("StopAnimator", 0.2f);
-	}
+    void MoveAnimation()
+    {
+        myLights[0].intensity = 8;
+
+        myLights[1].intensity = 8;
+        myAnimator.speed = 8;
+        Invoke("StopAnimator", 0.2f);
+    }
 	void StopAnimator()
 	{
-		myLight.intensity = 0;
-		myAnimator.speed = 0;
+		myLights[0].intensity = 0;
+        myLights[1].intensity = 0;
+        myAnimator.speed = 0;
 	}
 
 	void OnEnable()
